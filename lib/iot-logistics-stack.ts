41066418a41b0e7df236a536030f5edb6fdbf3a5
Kenneth Wu <kennethnoelwu@gmail.com>
2021-04-20 23:35:50 -0700

    Initial stream plus account setup

--- lib/iot-logistics-stack.ts
+++ lib/iot-logistics-stack.ts
@@ -1,9 +1,12 @@
 import * as cdk from '@aws-cdk/core';
-
+import * as kinesis from '@aws-cdk/aws-kinesis'
 export class IotLogisticsStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new kinesis.Stream(this, "MyFirstStream", {
+      streamName: "my-awesome-stream",
+      shardCount: 1,
+    });
   }
 }
